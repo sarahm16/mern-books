cdec4f68c9ac8f0e1d35490cf9575e96bf88e07d
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-26 18:50:37 -0800

    added a buy button to each book card that takes user to book on google.com

--- client/src/components/books.js
+++ client/src/components/books.js
@@ -38,21 +38,26 @@ function Books(props) {
 
         // </div>
         <div className='card'>
-                <div><img src={props.image} id='image' /></div>
+                <div className='image'><img src={props.image} id='image' /></div>
                 {/* <div className='row'>
                     {props.title}
                     <br />
                     {props.authors}
                 </div> */}
                 <div className='card-body'>
-                    <div className='card-title'>{props.title}</div>
-                    <div className='card-text'>
+                    {/* <div className='card-title'>{props.title}</div> */}
+                    <div className='row'>
                         <div className='col buttons'>
                             {props.type === 'saved' && <button className='delete-button inline' onClick={() => API.delete(props.id)}>Delete</button>}
                             {props.type !== 'saved' && <button className='save-button inline' onClick={() => API.save(props)}>Save</button>}
                         </div>
                         <div className='col buttons'>
                             <div><a href={props.infoLink}><button className='view-button inline'>View</button></a></div>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col buy-button-col'>
+                            <a href={props.infoLink} target ='_blank'><button className='buy'>Buy</button></a>
                         </div>
                     </div>
                 </div>
