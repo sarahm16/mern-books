2f88ead2ddb2f10bf5df6e9f0e8f8aefd6bd469d
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-28 12:22:12 -0800

    added save button

--- client/src/components/books.js
+++ client/src/components/books.js
@@ -7,17 +7,22 @@ function Books(props) {
     
     return(
         <div className='card'>
-                <div className='card-img-top image'><img src={props.image} id='image' /></div>
+                <div className='image'><img src={props.image} id='image' /></div>
                 <div className='card-body'>
                     <div className='about'>
                         <div className='title'>{props.title}</div>
-                        {props.authors !== '' && <div className='authors'>By {props.authors}</div>}
+                        {/* {props.authors !== '' && <div className='authors'>By {props.authors}</div>} */}
                     </div>
 
                     <div className='rating'>
                         <div className="stars-outer">
                             <div className="stars-inner" style={{width: ratingPercent}}></div>
                         </div>
+                    </div>
+
+                    <div className='buttons'>
+                        {props.type === 'saved' && <button className='delete-button inline' onClick={() => API.delete(props.id)}>Delete</button>}
+                        {props.type !== 'saved' && <button className='save-button inline' onClick={() => API.save(props)}>Save</button>}
                     </div>
 
                     {/* <div className='row'>
