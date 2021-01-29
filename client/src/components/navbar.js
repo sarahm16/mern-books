3f7cb6770383c3ac53069a291a4433990382ba78
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-29 13:18:49 -0800

    added bootstrap search bar to navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Navbar() {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Search</a> 
-            <a className="navbar-brand" href="/saved">Saved</a>
+        // <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        //     <a className="navbar-brand" href="/">Search</a> 
+        //     <a className="navbar-brand" href="/saved">Saved</a>
+        // </nav>
+        <nav className="navbar navbar-light bg-light justify-content-between">
+            <a className="navbar-brand" href='/saved'>Saved</a>
+            <form className="form-inline">
+                <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </nav>
     )
 }
