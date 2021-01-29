99fae9c2a70e7ec52a72bd176242208e18f24560
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-28 16:42:05 -0800

    added background image to header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,9 +4,17 @@ import '../App.css';
 function Header() {
     return(
         <div>
-            <div className='jumbotron border border-dark'>
+            {/* <img src='http://bookshelf.themerex.net/wp-content/uploads/2015/04/bigopenbook.jpg' style={{width: '100%'}} /> */}
+            {/* <div className='jumbotron'>
                 <h1>React Google Books</h1>
                 <h4>Search for and save books of interest</h4>
+            </div> */}
+            <div className='bg'>
+                <img src={require('../images/book.jpg')} style={{width: '100%'}} />
+            </div>
+            <div className='header'>
+                <div className='header-title'>React Google Books</div>
+                <h6>Search for and save books of interest</h6>
             </div>
         </div>
     )
