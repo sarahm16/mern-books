4b40cf3c0f8b5efd7a031fc53577e264770d7fa3
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-29 13:09:39 -0800

    redirect user to amazon product link when top book card is clicked on

--- client/src/components/topBook.js
+++ client/src/components/topBook.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 function TopBook(props) {
     let book = props.book;
+    console.log(book);
 
     return(
-        <div className='card'>
-            <div className='image'><img src={book['book_image']} id='image' /></div>
-            <div className='card-body'>
-                <div className='about'>
-                    <div className='title'>{book.title}</div>
-                    <div className='authors'>By {book.author}</div>
+        <a href={book['amazon_product_url']} target='_blank'>
+            <div className='card'>
+                <div className='image'><img src={book['book_image']} id='image' /></div>
+                <div className='card-body'>
+                    <div className='about'>
+                        <div className='title'>{book.title}</div>
+                        <div className='authors'>By {book.author}</div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     )
 }
 
