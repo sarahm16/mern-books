8bfceea71e89df72574ca7b4021e9574e5fc18fe
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-29 17:14:44 -0800

    bootstrap toggle navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,11 +4,6 @@ import '../App.css';
 function Header() {
     return(
         <div>
-            {/* <img src='http://bookshelf.themerex.net/wp-content/uploads/2015/04/bigopenbook.jpg' style={{width: '100%'}} /> */}
-            {/* <div className='jumbotron'>
-                <h1>React Google Books</h1>
-                <h4>Search for and save books of interest</h4>
-            </div> */}
             <div className='bg'>
                 <img src={require('../images/book.jpg')} style={{width: '100%'}} />
             </div>
