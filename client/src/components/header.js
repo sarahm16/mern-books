3077f73eb053b25ec4049a95c20d53176bc84618
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-01-30 14:19:31 -0800

    positioned header wrapper relative to header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Header() {
     return(
-        <div>
+        <div className='header-wrapper'>
             <div className='bg'>
                 <img src={require('../images/book.jpg')} style={{width: '100%'}} />
             </div>
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -34,7 +34,7 @@ class Navbar extends Component {
             //     </form>
             // </nav>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light py-0">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light py-0 w-100">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
