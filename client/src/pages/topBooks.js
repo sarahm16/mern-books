2e40d97d2a3369e19e2f930bf4a003adccef1ddc
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-04 14:43:40 -0800

    added dropdown menu to top sellers page

--- client/src/pages/topBooks.js
+++ client/src/pages/topBooks.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 import TopBook from '../components/topBook';
 
+import './search.css';
+
 class TopBooks extends Component {
     constructor() {
         super()
         this.state = {
-            bookList: []
+            bookList: [],
+            showMenu: false
         }
     }
 
@@ -16,11 +19,32 @@ class TopBooks extends Component {
         this.setState({bookList: bookList})
     }
 
+    showMenu = () => {
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+    }
+
     render() {
         return(
                 <div className='row'>
-                    <div className='row top-sellers-title'>
-                        TOP SELLERS
+                    <div className='row top-sellers-header'>
+                        <div className='top-sellers-title'>
+                            <h2>TOP SELLERS</h2>
+                            {/* TOP SELLERS */}
+                        </div>
+                        <div className='top-sellers-dropdown'>
+                            <div className="dropdown">
+                                <button onClick={this.showMenu} className="btn btn-secondary" type="button" id="dropdownMenuButton">
+                                    Categories
+                                </button>
+                                {this.state.showMenu && <div className="menu">
+                                    <a className="dropdown-item" href="#">Action</a>
+                                    <a className="dropdown-item" href="#">Another action</a>
+                                    <a className="dropdown-item" href="#">Something else here</a>
+                                </div>}
+                            </div>
+                        </div>
                     </div>
                     <div className='row'>
                         {this.state.bookList.length !== 0 && this.state.bookList.map(book => {
