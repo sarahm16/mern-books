7966e93814822817edce7be842330f1b36ae5f86
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-13 14:11:50 -0800

    added key prop to each book in results

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -53,22 +53,22 @@ class Results extends Component {
                     </div>}
 
                     <div className='row book-grid'>
-                    {/* {this.state.books.length == 0 && <LoadingBar />} */}
                     {this.state.books.length > 0 && 
                     this.state.books.map(book => {
+
                         //console.log(book.volumeInfo.industryIdentifiers[0].identifier);
                         let image;
                         let author;
                         let rating;
+                        let key = book.volumeInfo.industryIdentifiers[0].identifier;
 
                         //HANDLE UNDEFINED DATA
                         book.volumeInfo.imageLinks == undefined ? image = '' : image = book.volumeInfo.imageLinks.smallThumbnail;
                         book.volumeInfo.authors == undefined ? author = '' : author = book.volumeInfo.authors[0];
                         book.volumeInfo.averageRating == undefined ? rating = 2.5 : rating = book.volumeInfo.averageRating;
-                        console.log(rating);
                         
                         return(
-                            <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center'>
+                            <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center' key={key}>
                                 <Books title={book.volumeInfo.title}
                                 authors={author}
                                 image={image}
