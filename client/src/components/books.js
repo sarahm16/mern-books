7279b71f4b23e72ad1a7ed7d02e35b886ed72576
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-12 16:12:08 -0800

    created book saved toast that appears when user clicks save button and fades out after 4s

--- client/src/components/books.js
+++ client/src/components/books.js
@@ -1,48 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import API from '../utils/API';
 import './style.css';
 
-import axios from 'axios';
+class Books extends Component {
+    constructor() {
+        super();
+        this.state={
+            rating: 0,
+            showToast: false,
+            fadeOut: false
+        }
+    }
 
-function Books(props) {
-    let ratingPercent = props.rating / 5 * 100;
+    componentDidMount() {
+        console.log(this.props.rating)
+        let ratingPercent = this.props.rating / 5 * 100;
+        this.setState({rating: ratingPercent});
+    }
 
-    //console.log(props.image);
+    fadeOut = () => {
+        this.setState({fadeOut: true});
+        setTimeout(this.setState({showToast: false}), 3000);
+    }
 
-    async function deleteBook() {
+    saveBook = () =>  {
+        this.setState({showToast: true});
+        setTimeout(this.fadeOut, 1000);
+        API.save(this.props);
+    }
+
+    async deleteBook() {
         //props.updateSaved();
-        API.delete(props.id)
-
+        API.delete(this.props.id)
     }
 
-    function updateSaved() {
-        deleteBook().then(() => {props.updateSaved()})
+    updateSaved = () => {
+        this.deleteBook().then(() => {this.props.updateSaved()})
     }
+
+    render() {
     
-    return(
-            <div className='card'>
-                <a href={props.infoLink} target='_blank' className='google-link'></a>
-                <div className='image'><img src={props.image} id='image' /></div>
-                <div className='card-body'>
-                    <div className='about'>
-                        <div className='title'>{props.title}</div>
-                        {/* {props.authors !== '' && <div className='authors'>By {props.authors}</div>} */}
-                    </div>
-
-                    <div className='rating'>
-                        <div className="stars-outer">
-                            <div className="stars-inner" style={{width: ratingPercent}}></div>
+        return(
+                <div className='card'>
+                    {/* {this.state.showToast && <div className='toast'>Saved!!</div>} */}
+                    
+                    <a href={this.props.infoLink} target='_blank' className='google-link'></a>
+                    
+                    <div className='image'>
+                        {this.state.showToast && <div className={this.state.fadeOut ? 'saved-toast visible' : 'saved-toast fadeOut'}>BOOK SAVED</div>}
+                        <img src={this.props.image} id='image' /></div>
+                    <div className='card-body'>
+                        <div className='about'>
+                            <div className='title'>{this.props.title}</div>
+                            {/* {props.authors !== '' && <div className='authors'>By {props.authors}</div>} */}
                         </div>
-                    </div>
 
-                    <div className='buttons'>
-                        {props.type === 'saved' && <button className='delete-button inline' onClick={updateSaved}>Delete</button>}
-                        {props.type !== 'saved' && <button className='save-button inline' onClick={() => API.save(props)}>Save</button>}
-                    </div>
+                        <div className='rating'>
+                            <div className="stars-outer">
+                                <div className="stars-inner" style={{width: this.state.rating}}></div>
+                            </div>
+                        </div>
 
-                </div>
-        </div>
-    )
+                        <div className='buttons'>
+                            {this.props.type === 'saved' && <button className='delete-button inline' onClick={this.updateSaved}>Delete</button>}
+                            {this.props.type !== 'saved' && <button className='save-button inline' onClick={this.saveBook}>Save</button>}
+                        </div>
+
+                    </div>
+            </div>
+    )}
 }
 
 export default Books;
