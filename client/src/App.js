975fcf798c57c53fc68d195ce9b952b1724d5887
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-13 13:39:15 -0800

    incorporated lazy loading into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/header';
 import Search from './pages/search';
 import Navbar from './components/navbar';
-import Saved from './pages/saved';
-import Results from './components/results';
+//import Saved from './pages/saved';
+//import Results from './components/results';
+
+const Saved = lazy(() => import('./pages/saved'));
+const Results = lazy(() => import('./components/results'));
 
 function App() {
   return (
     <Router>
-    <div className="App">
-        {/* <Navbar /> */}
-        {/* <Header /> */}
-        <Route exact path='/' component={Search} />
-        <Route exact path='/saved' component={Saved} />
-        <Route exact path='/results/:title' component={Results} />
-    </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="App">
+            <Route exact path='/' component={Search} />
+            <Route exact path='/saved' component={Saved} />
+            <Route exact path='/results/:title' component={Results} />
+        </div>
+      </Suspense>
     </Router>
   );
 }
