2c5411636ceed200e0df282ccebceec5e77645eb
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-12 16:51:07 -0800

    removed unnecessary code. Added css styling to delete button

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -35,35 +35,35 @@ class Saved extends Component {
 
     render() {
         return(
-        <div>
-            <Navbar page='saved' />
-            <Header />
-            <div className='container-fluid'>
-                <div className='row'>
-                        <div className='saved-title'>
-                            {/* <h2>TOP SELLERS</h2> */}
-                            SAVED:
-                        </div>
-                    </div>
-                <div className='row'>
-                    {this.state.saved.map(book => {
-                        return(
-                            <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center'>
-                                <Books type='saved'
-                                id={book._id}
-                                title={book.title}
-                                authors={book.authors}
-                                image={book.image}
-                                description={book.description}
-                                infoLink={book.infoLink}
-                                rating={book.rating} 
-                                updateSaved={this.updateSaved}/>
+            <div>
+                <Navbar page='saved' />
+                <Header />
+                <div className='container-fluid'>
+                    <div className='row'>
+                            <div className='saved-title'>
+                                {/* <h2>TOP SELLERS</h2> */}
+                                SAVED:
                             </div>
-                        )
-                    })} 
+                        </div>
+                    <div className='row book-grid'>
+                        {this.state.saved.map(book => {
+                            return(
+                                <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center'>
+                                    <Books type='saved'
+                                    id={book._id}
+                                    title={book.title}
+                                    authors={book.authors}
+                                    image={book.image}
+                                    description={book.description}
+                                    infoLink={book.infoLink}
+                                    rating={book.rating} 
+                                    updateSaved={this.updateSaved}/>
+                                </div>
+                            )
+                        })} 
+                    </div>
                 </div>
             </div>
-        </div>
             
         )}
 }
