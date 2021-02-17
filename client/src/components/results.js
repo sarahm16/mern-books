9aaf5bc401c9e73f16b9b1bf9464ac41b3ff3c93
Sarah Carter <sarahmarie.carter@yahoo.com>
2021-02-16 20:48:35 -0800

    added no results div when totalItems === 0

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -64,33 +64,39 @@ class Results extends Component {
 
 
                     <div className='row book-grid'>
-                    {this.state.totalItems > 0 && 
-                    this.state.books.map(book => {
-
-                        //console.log(book.volumeInfo.industryIdentifiers[0].identifier);
-                        let image;
-                        let author;
-                        let rating;
-                        //let key = book.volumeInfo.industryIdentifiers[0].identifier;
-                        let key = Math.floor(Math.random() * 100000000000);
-
-                        //HANDLE UNDEFINED DATA
-                        book.volumeInfo.imageLinks == undefined ? image = '' : image = book.volumeInfo.imageLinks.smallThumbnail;
-                        book.volumeInfo.authors == undefined ? author = '' : author = book.volumeInfo.authors[0];
-                        book.volumeInfo.averageRating == undefined ? rating = 2.5 : rating = book.volumeInfo.averageRating;
-                        
-                        return(
-                            <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center' key={key}>
-                                <Books title={book.volumeInfo.title}
-                                authors={author}
-                                image={image}
-                                description={book.volumeInfo.description}
-                                infoLink={book.volumeInfo.infoLink}
-                                rating={rating}
-                                id={key} />
+                        {this.state.totalItems === 0 &&
+                            <div>
+                                No Results
                             </div>
-                        )
-                    })}
+                        }
+                        {this.state.totalItems > 0 && 
+                            this.state.books.map(book => {
+
+                                //console.log(book.volumeInfo.industryIdentifiers[0].identifier);
+                                let image;
+                                let author;
+                                let rating;
+                                //let key = book.volumeInfo.industryIdentifiers[0].identifier;
+                                let key = Math.floor(Math.random() * 100000000000);
+
+                                //HANDLE UNDEFINED DATA
+                                book.volumeInfo.imageLinks == undefined ? image = '' : image = book.volumeInfo.imageLinks.smallThumbnail;
+                                book.volumeInfo.authors == undefined ? author = '' : author = book.volumeInfo.authors[0];
+                                book.volumeInfo.averageRating == undefined ? rating = 2.5 : rating = book.volumeInfo.averageRating;
+                                
+                                return(
+                                    <div className='col-lg-3 col-6 col-md-6 book-card d-flex justify-content-center' key={key}>
+                                        <Books title={book.volumeInfo.title}
+                                        authors={author}
+                                        image={image}
+                                        description={book.volumeInfo.description}
+                                        infoLink={book.volumeInfo.infoLink}
+                                        rating={rating}
+                                        id={key} />
+                                    </div>
+                                )
+                            })
+                        }
                     </div>               
                 </div>
             </div>
